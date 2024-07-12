719f50fa00d7f664c021909da8ba8e4abeef6f0d
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-07-12 23:08:21 +0100

    chore: optimize font loading and splash screen handling

--- App.tsx
+++ App.tsx
@@ -1,8 +1,34 @@
 
+import { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+} from '@expo-google-fonts/montserrat';
 import AppNavigationContainer from './src/navigation/AppNavigationContainer';
 
+
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded] = useFonts({  
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null; // Render nothing while the splash screen is visible
+  }
   return (
     <SafeAreaProvider>
     <AppNavigationContainer />
