4c740a11459d7db0f22717c23549ec32f75f1cf2
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-07-13 22:58:55 +0100

    chore: Update SVG imports and add new SVG file

--- assets/svg/index.js
+++ assets/svg/index.js
@@ -1,3 +1,5 @@
-export {default as Home} from './Home.svg';
-export {default as CartIcon} from './CartIcon.svg';
-export {default as CheckoutIcon} from './CheckoutIcon.svg';
+export { default as Home } from "./Home.svg";
+export { default as CartIcon } from "./CartIcon.svg";
+export { default as CheckoutIcon } from "./CheckoutIcon.svg";
+export { default as Card } from "./Card.svg";
+export { default as Dash } from "./Dash.svg";
