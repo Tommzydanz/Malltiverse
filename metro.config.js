5fb003a7c7b6214661e7c9b99b6814d243e1fe27
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-07-13 03:31:41 +0100

    chore: Update Metro bundler configuration to support SVG files

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
@@ -6,13 +6,13 @@ const { transformer, resolver } = config;
 
 config.transformer = {
   ...transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
 };
 
 config.resolver = {
   ...resolver,
-  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-  sourceExts: [...resolver.sourceExts, 'svg'],
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"],
 };
 
 module.exports = config;
