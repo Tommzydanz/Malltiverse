f1224748f1840431b55f5f502fed297f395fa58e
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-07-13 22:59:54 +0100

    refactor: Update CartItem component to improve code readability and organization

--- src/components/cart-item/CartItem.tsx
+++ src/components/cart-item/CartItem.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { CartItemProps } from "./interface";
+import colors from "../../configs/colors.config";
 
 const CartItem: CartItemProps = ({
   cart,
@@ -20,130 +21,131 @@ const CartItem: CartItemProps = ({
       onRemoveFromCart(cart.id);
     }
   };
+
   return (
-    <View style={styles.cartItem}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-evenly",
-          alignItems: "flex-start",
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: `https://api.timbu.cloud/images/${cart.photos[0].url}`,
         }}
-      >
-        <Image
-          source={cart.photos[0].url[0]}
-          style={{
-            width: 100,
-            height: 100,
-            borderRadius: 8,
-            justifyContent: "flex-start",
-          }}
-          resizeMode="contain"
-        />
-        <Text style={styles.productName} numberOfLines={5} lineBreakMode="clip">
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <View style={styles.detailsContainer}>
+        <Text style={styles.name} numberOfLines={2}>
           {cart.name}
         </Text>
-        <Text style={styles.priceText}>
-          ₦ {(cart.quantity * cart.current_price[0].NGN[0]).toLocaleString("en-US")}
+        <Text style={styles.description} numberOfLines={1}>
+          {/* Add a description field to your cart object if not present */}
+          {cart.description || "Product Description"}
         </Text>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginLeft: 12,
-        }}
-      >
-        <TouchableOpacity
-          onPress={() => onRemoveFromCart(cart.id)}
-          style={styles.removeButton}
-        >
-          <Ionicons name="trash-outline" color={"red"} size={24} />
-          <Text style={styles.removeText}>Remove</Text>
-        </TouchableOpacity>
         <View style={styles.quantityContainer}>
           <TouchableOpacity
             style={[
-              styles.button,
+              styles.quantityButton,
               cart.quantity === 1 && styles.disabledButton,
             ]}
             onPress={handleDecrement}
             disabled={cart.quantity === 1}
           >
-            <AntDesign name="minus" size={18} color="white" />
+            <Text style={styles.quantityCounter}>-</Text>
           </TouchableOpacity>
-          <Text style={styles.countText}>{cart.quantity}</Text>
-          <TouchableOpacity style={styles.button} onPress={handleIncrement}>
-            <AntDesign name="plus" size={18} color="white" />
+          <Text style={styles.quantity}>{cart.quantity}</Text>
+          <TouchableOpacity
+            style={styles.quantityButton}
+            onPress={handleIncrement}
+          >
+            <Text style={styles.quantityCounter}>+</Text>
           </TouchableOpacity>
         </View>
+      </View>
+      <View style={styles.rightContainer}>
+        <TouchableOpacity
+          onPress={() => onRemoveFromCart(cart.id)}
+          style={styles.removeButton}
+        >
+          <Ionicons
+            name="trash-outline"
+            size={18}
+            color={colors.secondaryFade}
+          />
+        </TouchableOpacity>
+        <Text style={styles.price}>
+          N{" "}
+          {(cart.quantity * cart.current_price[0].NGN[0]).toLocaleString(
+            "en-US",
+          )}
+        </Text>
       </View>
     </View>
   );
 };
 
-export default CartItem;
-
 const styles = StyleSheet.create({
-  cartItem: {
-    marginBottom: 8,
-  },
-  productName: {
-    fontSize: 16,
-    lineHeight: 20,
-    width: "40%",
-    flexWrap: "wrap",
-  },
-  priceText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    lineHeight: 20,
-    flexWrap: "wrap",
-  },
-  removeButton: {
+  container: {
     flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center",
+    padding: 16,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: colors.secondaryFade,
+    marginBottom: 21,
   },
-  removeText: {
-    color: "#fc2828",
-    fontSize: 16,
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 8,
+    marginRight: 16,
   },
-  totalContainer: {
-    marginTop: 16,
-    alignItems: "center",
+  detailsContainer: {
+    flex: 1,
+    justifyContent: "space-between",
   },
-  totalText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 8,
+  name: {
+    fontSize: 12,
+    lineHeight: 15,
+    fontFamily: "Montserrat_600SemiBold",
   },
-  checkoutButton: {
-    backgroundColor: "orange",
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-    borderRadius: 4,
-  },
-  checkoutButtonText: {
-    color: "white",
-    fontWeight: "bold",
+  description: {
+    fontSize: 11,
+    fontFamily: "Montserrat_400Regular",
+    color: colors.secondary,
   },
   quantityContainer: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
-    marginTop: 4,
+    marginTop: 8,
   },
-  button: {
-    backgroundColor: "orange",
-    padding: 5,
-    borderRadius: 10,
-    marginHorizontal: 20,
+  quantityButton: {
+    width: 20,
+    height: 20,
+    borderWidth: 1,
+    borderColor: colors.secondary,
+  },
+  quantityCounter: {
+    fontFamily: "Montserrat_500Medium",
+    fontSize: 17,
+    top: -2,
+    alignSelf: "center",
   },
   disabledButton: {
     opacity: 0.5,
   },
-  countText: {
-    fontSize: 18,
+  quantity: {
+    fontSize: 12,
+    fontFamily: "Montserrat_500Medium",
+    marginHorizontal: 12,
+  },
+  rightContainer: {
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+  },
+  removeButton: {
+    padding: 4,
+  },
+  price: {
+    fontSize: 16,
+    fontFamily: "Montserrat_500Medium",
   },
 });
+
+export default CartItem;
