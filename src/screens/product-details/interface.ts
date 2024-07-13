521cf231821c9f4c0d2c44c24fe853ca9e6ab01d
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-07-13 23:08:57 +0100

    refactor: Remove unused ProductDetails screen component

--- src/screens/product-details/interface.ts
+++
@@ -1,8 +0,0 @@
-import { RouteProp } from "@react-navigation/native";
-import { RootStackParamList } from "../../navigation/interface";
-import { StackScreenProps } from "@react-navigation/stack";
-
-  
-  
-export type ProductDetailProps =  React.FC<StackScreenProps<RootStackParamList, 'ProductDetails'>>;
-  
