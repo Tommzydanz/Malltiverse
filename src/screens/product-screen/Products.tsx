a6f3188d6357bf9757527907e9830adedb5617af
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-07-13 23:09:07 +0100

    feat: Update Products screen component with lazy loading for images

--- src/screens/product-screen/Products.tsx
+++ src/screens/product-screen/Products.tsx
@@ -1,12 +1,23 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { View, FlatList, StyleSheet, Text, TouchableOpacity} from "react-native";
-import ProductItem from "../../components/product-item/ProductItem";
-import { Ionicons } from "@expo/vector-icons"
-import { getProducts, Product } from "../../api/products";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import colors from "../../configs/colors.config";
+import ProductCategory from "../../components/product-category/ProductCategory";
 import { Loader } from "../../components/Loader/Loader";
+import { getProductsWithRatings, Product } from "../../api/products";
 
+interface ProductsScreenProps {
+  addToCart: (product: Product) => void;
+}
 
-const Products: React.FC = () => {
+const Products: React.FC<ProductsScreenProps> = ({ addToCart }) => {
   const [products, setProducts] = useState<Product[] | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -17,14 +28,14 @@ const Products: React.FC = () => {
   const fetchProducts = useCallback(async () => {
     setIsLoading(true);
     try {
-      const result = await getProducts();
+      const result = await getProductsWithRatings();
       if (result && result.items) {
         setProducts(result.items);
       } else {
-        setError('Failed to fetch products');
+        setError("Failed to fetch products");
       }
     } catch (err) {
-      setError('An error occurred while fetching products');
+      setError("An error occurred while fetching products");
     }
     setIsLoading(false);
   }, []);
@@ -32,14 +43,16 @@ const Products: React.FC = () => {
   // Fetch products on component mount
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [fetchProducts]);
 
   // Render loading state
   if (isLoading) {
     return (
       <View style={styles.otherContainer}>
-        <Loader size={'large'} color={'orange'} />
-        <Text>Fetching Products...</Text>
+        <Loader size={"large"} color={colors.primary} />
+        <Text style={{ fontFamily: "Montserrat_500Medium" }}>
+          Fetching Products...
+        </Text>
       </View>
     );
   }
@@ -48,8 +61,8 @@ const Products: React.FC = () => {
   if (error) {
     return (
       <View style={styles.otherContainer}>
-        <Ionicons name="alert-circle-outline" size={40} color="red" />
-        <Text>Error: {error}</Text>
+        <Ionicons name="alert-circle-outline" size={40} color={colors.danger} />
+        <Text style={styles.tryAgainButtonText}>Error: {error}</Text>
         <TouchableOpacity style={styles.tryAgainButton} onPress={fetchProducts}>
           <Text style={styles.tryAgainButtonText}>Try Again</Text>
         </TouchableOpacity>
@@ -58,41 +71,41 @@ const Products: React.FC = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={products || []}
-        showsVerticalScrollIndicator={false}
-        numColumns={2}
-        renderItem={({ item }) => (
-          <ProductItem product={item} />
-        )}
-        keyExtractor={(item) => item.id}
-        contentContainerStyle={{ gap: 10 }}
+    <ScrollView style={styles.container}>
+      <Image
+        source={require("../../../assets/big-banner.png")}
+        style={styles.banner}
       />
-    </View>
+      <ProductCategory products={products} addToCart={addToCart} />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
-    padding: 16,
+    backgroundColor: colors.whiteFade,
+    padding: 22,
+  },
+  banner: {
+    width: "100%",
+    borderRadius: 10,
   },
   otherContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: colors.whiteFade,
+    justifyContent: "center",
+    alignItems: "center",
   },
   tryAgainButton: {
-    backgroundColor: 'orange',
+    backgroundColor: colors.primary,
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 5,
   },
   tryAgainButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: colors.secondary,
+    fontFamily: "Montserrat_500Medium",
   },
 });
 
