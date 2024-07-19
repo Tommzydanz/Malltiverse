2701cf7ae7959368c838eec700e95c18517f557e
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-07-19 07:55:32 +0100

    fix: update svg

--- assets/svg/index.js
+++ assets/svg/index.js
@@ -1,5 +1,5 @@
 export { default as Home } from "./Home.svg";
 export { default as CartIcon } from "./CartIcon.svg";
 export { default as CheckoutIcon } from "./CheckoutIcon.svg";
-export { default as Card } from "./Card.svg";
 export { default as Dash } from "./Dash.svg";
+export { default as History } from "./History.svg";
