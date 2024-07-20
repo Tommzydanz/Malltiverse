9deffff65e22dc01c36b9ec91262d0cda81e8147
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-07-20 20:51:58 +0100

    feat: Add formatDate utility function for formatting dates

---
+++ src/utils/date.ts
@@ -0,0 +1,5 @@
+// Format date utils
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return `${date.getDate().toString().padStart(2, "0")}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getFullYear()}`;
+};
